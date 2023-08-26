a02304a81d405b2bb67681fe812546ef09a75554
kipngenohaaron <kipngenohaaron@gmail.com>
2023-08-26 12:27:37 +0300

    assignment completed

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -2,3 +2,20 @@
 
 class MyString:
   pass
+  def __init__(self, value = ""):
+    self.value = value
+
+  def is_sentence(self):
+    return self.value.endswith(".")
+  
+  def is_question(self):
+    return self.value.endswith("?")
+  
+  def is_exclamation(self):
+    return self.value.endswith("!")
+  
+  def count_sentences(self):
+   if not self.value:
+     return 0
+   sentences = [sentence.strip() for sentence in self.value.split(".") if sentence.strip()]
+   return len(sentences)  
